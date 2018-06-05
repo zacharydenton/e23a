686d90099952ed5ebc77408c3398cb4240e59008
Zach Denton <z@chdenton.com>
2018-06-04 21:12:15 -0700

    Add countdown before play commences.

--- fifty-bird/main.lua
+++ fifty-bird/main.lua
@@ -7,6 +7,7 @@ require 'PipePair'
 
 require 'StateMachine'
 require 'states/BaseState'
+require 'states/CountdownState'
 require 'states/PlayState'
 require 'states/ScoreState'
 require 'states/TitleScreenState'
@@ -53,7 +54,8 @@ function love.load()
   gStateMachine = StateMachine({
     ['title'] = function() return TitleScreenState() end,
     ['play'] = function() return PlayState() end,
-    ['score'] = function() return ScoreState() end
+    ['score'] = function() return ScoreState() end,
+    ['countdown'] = function() return CountdownState() end
   })
   gStateMachine:change('title')
 
